ca2ccf02d7d6f2e7727c58dd713a980be6959c77
qbert0 <126874637+qbert0@users.noreply.github.com>
2023-12-30 03:40:15 +0700

    fix

---
+++ app/api/myapi.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+    runtime : "edge",
+}
+
+const handler = (req : NextRequest) => {
+    return NextResponse.json({
+        name : "hello"
+    })
+}
